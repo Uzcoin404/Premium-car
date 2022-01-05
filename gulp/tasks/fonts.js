6eebd359e780a53f6385037d656942be45c786ec
Uzcoin404 <uzcointg@gmail.com>
2022-01-05 12:45:37 +0500

    end of work

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -2,7 +2,7 @@ var ttf2woff2 = require('gulp-ttf2woff2');
 module.exports = () => 
 $.gulp.task('fonts', () =>
 $.gulp.src($.path.src.fonts)
-.pipe($.gulp.dest($.path.dist.fonts))
-.pipe($.gp.ttf2woff2())
-.on('end', $.bs.reload)
+// .pipe($.gulp.dest($.path.dist.fonts))
+// .pipe($.gp.ttf2woff2())
+// .on('end', $.bs.reload)
 )
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,35 @@
-document.body.style.background = 'green';
+const sectionImg = document.querySelectorAll('.section_img');
+const section = document.querySelectorAll('.section');
+for (let i = 0; i < section.length; i++) {
+    sectionImg[i].addEventListener('mouseover', function(){
+        section[i].classList.add('active');
+    });
+    sectionImg[i].addEventListener('mouseout', function(){
+        section[i].classList.remove('active');
+    });
+}
+
+const navLink = document.querySelectorAll('.nav_link a');
+for (let i = 0; i < navLink.length; i++) {
+    let navtagName = navLink[i].getAttribute('href');
+    navLink[i].href = '#' + navtagName;
+}
+
+const nav = document.querySelector('.nav');
+const navMenu = document.querySelector('.nav_menu');
+const navList = document.querySelector('.nav_list');
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 100) {
+        $("nav").addClass("fixed");
+    } else {
+        $("nav").removeClass("fixed");
+    }
+});
+
+window.addEventListener('click', function(e){
+    if (e.target != navMenu && e.target != navMenu.querySelector('i') && e.terget != nav) {
+        navList.classList.remove('active');
+    } else{
+        navList.classList.add('active');
+    }
+});
